1bc0196ca74d896ebe22bf49e12d860475e38511
Kevin Pearson <kevin_pearson@cable.comcast.com>
2022-12-11 08:36:04 -0800

    Add method to retrieve all privacy settings

--- test/typescript/privacy.test.ts
+++ test/typescript/privacy.test.ts
@@ -3,39 +3,6 @@ import { Privacy } from "../../dist/lib/firebolt-manage";
 
 let listenerId: number;
 
-test("Able to get TypeScript listenerId", () => {
-  return Privacy.listen("allowAppDataCollectionChanged", () => {}).then(
-    (id: number) => {
-      listenerId = id;
-      console.log(listenerId);
-      expect(listenerId > 0).toBe(true);
-    }
-  );
-});
-
-test("privacy.once() for allowAppDataCollectionChanged event", () => {
-  Privacy.once;
-  return Privacy.once("allowAppDataCollectionChanged", () => {}).then(
-    (res: number) => {
-      expect(res > 0).toBe(true);
-    }
-  );
-});
-
-test("privacy.allowAppDataCollection()", () => {
-  return Privacy.allowAppDataCollection('test').then((res: boolean) => {
-    // TODO: fix
-    expect(true).toBe(true);
-  });
-});
-
-test("privacy.allowAppEntitlementCollection()", () => {
-  return Privacy.allowAppEntitlementCollection('test').then((res: boolean) => {
-    // TODO: fix
-    expect(true).toBe(true);
-  });
-});
-
 test("privacy.allowResumePoints()", () => {
   return Privacy.allowResumePoints().then((res: boolean) => {
     // TODO: fix
